f128eb84a1d2181a9a4116a833f011a839570e59
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2020-11-27 12:47:20 -0600

    se agrego bootstrap, FA, se creo el navbar

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,57 @@
 import React from 'react'
-import { Navbar } from './Navbar'
+import { NavbarPage } from './Navbar'
+import logo from './assets/logo-diamond.svg'
+
 
 export const Main = () => {
     return (
         <>
-            <Navbar />
-            <h1>Pantalla principal</h1>
+            <NavbarPage />
+            <div id="principal" className="container-fluid h-100">
+            <h1 className="text-center mt-1">Te hacemos feliz en 8 bits!</h1>
+                <div className="row h-100">
+                    <div className="col-5">
+                        <img src={logo} alt="logo" width="200px"></img>
+                    </div>
+                    <div className="col-5">
+                        
+                    </div>
+                </div>
+            </div>
+            <div id="conocenos" className="container-fluid h-100">
+                <div className="row h-100">
+                    <div className="col-12">
+                        <div className="row justify-content-center align-items-center">
+                            <div className="col-1">
+                                <div className="cir"></div>
+                            </div>
+                            <div className="col-11">
+                                <h1>Conocenos</h1>
+                            </div>
+                            <div className="col-12 mt-2">
+                                <h2>¿Quienes somos?</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="contacto" className="container-fluid h-100">
+                <div className="row h-100">
+                    <div className="col-12">
+                        <div className="row justify-content-center align-items-center">
+                            <div className="col-1">
+                                <div className="cir"></div>
+                            </div>
+                            <div className="col-11">
+                                <h1>Contacto</h1>
+                            </div>
+                            <div className="col-12 mt-2">
+                                <h2>¿Quienes somos?</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit'
 import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
-export const Navbar = () => {
+export const NavbarPage = () => {
+
     return (
-        <header>
-            <h1>
-                <Link to="/">Diamond Pixel</Link>
-            </h1>
-            <button>
-                <Link to="/Designer">Crea tu diseño</Link>
-            </button>
-        </header>
+        <>
+            <header>
+                <Navbar className="nav-custom" expand="lg">
+                    <Navbar.Brand ><Link className="titlenav" to="/">DIAMOND PIXEL</Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#principal">
+                                <span className="link-custom">Principal</span>
+                            </Nav.Link>
+                            <Nav.Link href="#conocenos">
+                                <span className="link-custom">Conocenos</span>
+                            </Nav.Link>
+                            <Nav.Link href="#contacto">
+                                <span className="link-custom">Contacto</span>
+                            </Nav.Link>
+                            <Link to="/Designer" className="btn btn-custom">
+                                Crea tu diseño
+                            <FontAwesomeIcon className="ml-2" icon={faEdit} />
+                            </Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </header>
+        </>
     )
 }
