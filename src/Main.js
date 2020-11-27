e11a371f89edafa5bbfdaed089ad507e5dc9aea1
ktdral14 <angel.catedral.1410@gmail.com>
2020-11-26 21:26:16 -0600

    :tada: Page basic structure

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navbar } from './Navbar'
+
+export const Main = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Pantalla principal</h1>
+        </>
+    )
+}
