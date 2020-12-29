a0984fafc88210b09a76ecb581ee87c579c2403e
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2020-12-29 12:14:27 -0600

    pixel art funcional

--- src/Designer.jsx
+++ src/Designer.jsx
@@ -1,26 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavbarPage } from './NavbarPage'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPen } from '@fortawesome/free-solid-svg-icons/faPen'
-import { faSquare } from '@fortawesome/free-solid-svg-icons/faSquare'
-import { faCircle } from '@fortawesome/free-solid-svg-icons/faCircle'
-import { faFillDrip } from '@fortawesome/free-solid-svg-icons/faFillDrip'
-import { faEraser } from '@fortawesome/free-solid-svg-icons/faEraser'
+import { faPen, faFillDrip, faEraser } from '@fortawesome/free-solid-svg-icons'
+import html2canvas from 'html2canvas'
 
 export const Designer = () => {
+    const offCell = {
+        on: false,
+        color: '#ffffff'
+    }
+    const initialCells = Array.from({length: 30*30}, () => offCell);
+    const [cells, setCells] = useState(initialCells);
+    const [colorActual, setColorActual] = useState('#ffffff');
+    const [colores] = useState([
+        '#026d5b',
+        '#2ba461',
+        '#48e387',
+        '#8ef2c0',
+        '#ffffff',
+        '#f8cca7',
+        '#fc353c',
+        '#f8404c',
+        '#ff6c29',
+        '#ebbb3b',
+        '#efe286',
+        '#f4ce90',
+        '#ff6bab',
+        '#fc70c7',
+        '#fbd1df',
+        '#a88cd6',
+        '#af55d0',
+        '#9e33a3',
+        '#4dabd1',
+        '#548cad',
+        '#4db0e9',
+        '#73ccee',
+        '#0010be',
+        '#452586',
+        '#000000',
+        '#592300',
+        '#601706',
+        '#ad270c',
+        '#c97455',	
+        '#b2b2b2',
+        '#fe6903',
+        '#505050'
+    ]);
+    const onChangeColor = (value) => {
+        console.log(value);
+        setColorActual(value);
+    }
+    const updateCell = (index) => (e) => {
+        e.preventDefault();
+        if (e.buttons === 1 || e.buttons === 2) {
+            setCells(
+                cells.map((cell, cellIndex) => {
+                if (cellIndex === index) {
+                    if (e.buttons === 1) {
+                        return {
+                            on: true,
+                            color: colorActual
+                        }
+                    }
+                    return offCell;
+                }
+                return cell;
+            }));
+        }
+    };
+    const saveCanvas = () => {
+        html2canvas(document.getElementById('table'), {backgroundColor: null}).then(canvas => {
+            const link = document.createElement("a");
+                document.body.appendChild(link);
+                link.download = "lienzo.png";
+                link.href = canvas.toDataURL("image/png");
+                link.target = '_blank';
+                link.click();
+                document.body.removeChild(link);
+        })
+    }
     return (
         <>
             <NavbarPage inicio={false} />
             <div id="desinger" className=" h-100 container-fluid padre">
                 <div className="row">
                     <div className="col-12 d-flex justify-content-center align-text-center">
-                        <label htmlFor="" className="eslogan2">!Crea¡!Juega¡!Diseña¡</label>
+                        <label htmlFor="" className="eslogan2">¡Crea!¡Juega!¡Diseña!</label>
                     </div>
-                    
+                    <div className="col-12">
+                        <div className="current-color" style={{background: colorActual}}></div>
+                    </div>
                 </div>
                 <div className="row content">
                     <div className="col-lg-5 col-md-12 col-sm-6 d-flex justify-content-center imagen">
-                        <img src="https://thumbs.dreamstime.com/b/cuadr%C3%ADcula-de-papel-cuadrada-sobre-un-patr%C3%B3n-transparente-fondo-hoja-mil%C3%ADmetro-168063673.jpg" width="460" height="450"></img>
+                        <div id="table" className="grid">
+                            { 
+                            cells.map((cell, index) =>
+                                                    <div 
+                                                        style={{background: cell.on ? cell.color: '#ffffff'}} 
+                                                        onClick={updateCell(index)} key={index} 
+                                                        onMouseDown = {updateCell(index)}
+                                                        onMouseOver = {updateCell(index)}
+                                                        onContextMenu = {(e) => e.preventDefault()}
+                                                        className="cell">
+                                                    </div>) 
+                            }
+                        </div>
                     </div>
                     <div className="col-2  d-flex justify-content-center alig-items-center align-text-center columnaColores">
                         
@@ -29,11 +114,9 @@ export const Designer = () => {
                         <label className="datos3">Herramientas</label>
                             <div className="herramientas">
                             <ul>
-                                <li className="mb-3 mt-4"><span><FontAwesomeIcon className="" icon={faPen} />   &nbsp;&nbsp;           </span>Lapiz</li>
-                                <li className="mb-3"><span><FontAwesomeIcon className="" icon={faSquare} />    &nbsp;&nbsp;            </span>Cuadrado</li>
-                                <li className="mb-3"><span><FontAwesomeIcon className="" icon={faCircle} />        &nbsp;&nbsp;        </span>Ovalo</li>
-                                <li className="mb-3"><span><FontAwesomeIcon className="" icon={faFillDrip} />    &nbsp;&nbsp;          </span>Rellenar</li>
-                                <li className="mb-3"><span><FontAwesomeIcon className="" icon={faEraser} />     &nbsp;&nbsp;           </span>Borrar</li>
+                                <li style={{cursor: 'pointer'}} className="mb-3 mt-4"><span><FontAwesomeIcon className="mr-2" icon={faPen} /></span>Click izq</li>
+                                <li style={{cursor: 'pointer'}} className="mb-3"><span><FontAwesomeIcon className="mr-2" icon={faEraser} /></span>Click der</li>
+                                <li style={{cursor: 'pointer'}} className="mb-3"><span><FontAwesomeIcon className="mr-2" icon={faFillDrip} /></span>Rellenar</li>
                             </ul>
                             </div>
                         </form>
@@ -43,7 +126,16 @@ export const Designer = () => {
                         <form className="formColores">
                         <label className="datos2">Colores disponibles</label>
                             <div className="herramientas2">
-                                <img src="https://miro.medium.com/max/670/1*f5oJNohkk3B1B8WQsAXOtQ.png" alt="" width="120px" height="80"/>
+                                <div className="grid-colores">
+                                    { 
+                                        colores.map((value, key) => 
+                                                        <div className="cell-color" 
+                                                        key={key}
+                                                        style={{background : value, outline: '1px solid ' + value}}
+                                                        onClick={() => onChangeColor(value)}>
+                                                        </div>) 
+                                    }
+                                </div>
                             </div>
                         </form>
                         </div>
@@ -94,7 +186,7 @@ export const Designer = () => {
                             <button className="btn btn-enviar2">Enviar</button>
                         </div>
                         <div className="text-center col-6">
-                            <button className="btn btn-enviar2">Descargar</button>
+                            <button className="btn btn-enviar2" type="button" onClick= {saveCanvas}>Descargar</button>
                         </div>
                         </div>
                         </div>
